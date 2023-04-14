9c54e00104b0658283571cdc00231bf20c298d3f
babu <yanakibabu@gmail.com>
2023-04-14 15:48:26 +0530

    form_code  code is added

---
+++ src/form2.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+const App = () =>{
+  const [data,setData] = useState({
+    name:"",
+    email:"",
+    password:""
+  });
+  const {name,email,password} = data;
+
+  const handler = (e) =>{
+    setData({...data, [e.target.name]:[e.target.value]})
+  }
+
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    console.log(e.target.value);
+
+  }
+ return(
+  <>
+ <h2>login form</h2>
+ <div style={{ backgroundColor:"lightgrey", textAlign:"center", width:"500px", height:"200px", borderRadius:"25px"}}>
+ <form>
+  
+   <input type="text"  placeholder="ENTER NAME" name="name" value={name} onChange={handler} /><br/><br/>
+
+   <input type="email" placeholder="ENTER EMAIL" name="email" value={email} onChange={handler} /><br/><br/>
+  
+   <input type="password" placeholder=" ENTER PASSWORD" name="password" value={password} onChange={handler} /><br/><br/>
+
+    <button>submit</button>
+ </form>
+ </div>
+  </>
+ )
+}
+export default App;
